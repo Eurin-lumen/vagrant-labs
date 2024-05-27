de9f1f82a05b80875833d0b001c04a293388abba
Eurin HASH <eurindelagoldcoast@gmail.com>
2024-05-27 11:37:09 +0100

    new test

--- lab10/install_jenkins.sh
+++ lab10/install_jenkins.sh
@@ -1,18 +1,29 @@
 #!/bin/bash
-yum -y update
-yum -y install epel-release
 
-# install ansible
-yum -y install ansible
+# Mise à jour des paquets
+sudo yum update -y
 
-# retrieve ansible code
-yum -y install git
-git clone https://github.com/diranetafen/cursus-devops.git
-cd cursus-devops/ansible
-ansible-galaxy install -r roles/requirements.yml
-ansible-playbook install_docker.yml
-sudo usermod -aG docker vagrant
-cd ../jenkins
-/usr/local/bin/docker-compose up -d
-echo "For this Stack, you will use $(ip -f inet addr show enp0s8 | sed -En -e 's/.*inet ([0-9.]+).*/\1/p') IP Address"
-echo "login : user and password : bitnami"
+# Installation des dépendances nécessaires
+sudo yum install -y yum-utils device-mapper-persistent-data lvm2
+
+# Ajout du dépôt Docker
+sudo yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
+
+# Installation de Docker
+sudo yum install -y docker-ce docker-ce-cli containerd.io
+
+# Démarrage de Docker
+sudo systemctl start docker
+sudo systemctl enable docker
+
+# Téléchargement de Docker Compose
+sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+
+# Ajout des permissions d'exécution à Docker Compose
+sudo chmod +x /usr/local/bin/docker-compose
+
+# Vérification de l'installation
+docker-compose --version
+
+echo 'For this Stack, you will use 192.168.50.4 IP Address'
+echo 'login : user and password : bitnami'
